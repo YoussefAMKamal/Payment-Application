313caef47a8a74fa726bd3fbc936c8906cca6a01
YoussefAMKamal <yo20002012@gmail.com>
2022-11-10 21:42:02 +0200

    1.2.0

--- Payment Project/Server/server.c
+++ Payment Project/Server/server.c
@@ -6,13 +6,20 @@ ST_accountsDB_t account_Data[255] = { 0 };
 ST_transaction_t trans_Data[255] = { 0 };
 uint8_t state[20];
 uint8_t store = 0;
+uint32_t num = 0;
+
+ST_accountsDB_t acc;
+ST_transaction_t tD;
 
 EN_transState_t recieveTransactionData(ST_transaction_t* transData)
 {
 	if (isValidAccount(&transData->cardHolderData, &account_Data) == ACCOUNT_NOT_FOUND)
 		return FRAUD_CARD;
 	if (isAmountAvailable(&transData->terminalData, &account_Data) == LOW_BALANCE)
+	{
+		printf("fffffffffff\n");
 		return DECLINED_INSUFFECIENT_FUND;
+	}
 	if(isBlockedAccount(&account_Data) == BLOCKED_ACCOUNT)
 		return DECLINED_STOLEN_CARD;
 	if (saveTransaction(transData) == SAVING_FAILED)
@@ -44,7 +51,7 @@ EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accoun
 
 EN_serverError_t isBlockedAccount(ST_accountsDB_t* accountRefrence)
 {
-	if(accountRefrence->state == RUNNING)
+	if(accountRefrence->state != BLOCKED)
 		return SERVER_OK;
 	else
 	{
@@ -55,6 +62,7 @@ EN_serverError_t isBlockedAccount(ST_accountsDB_t* accountRefrence)
 
 EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t* accountRefrence)
 {
+	account_Data[num] = accountRefrence[num++];
 	if (termData->transAmount > accountRefrence->balance)
 	{
 		//printf("Error: Amount In Balance Is Not Enough\n");
@@ -123,12 +131,11 @@ void recieveTransactionDataTest(void)
 	account_Data->state = RUNNING;
 	account_Data->balance = 3000.0;
 
-	ST_transaction_t tD;
-
 	printf("Test Case 1:\n\
 	Input Data: No input data\n\
 	Expected Result: Error\n\
 	Actual Result: ");
+
 	strcpy(tD.cardHolderData.cardExpirationDate, "\0");
 	strcpy(tD.cardHolderData.cardHolderName, "\0");
 	strcpy(tD.cardHolderData.primaryAccountNumber, "\0");
@@ -150,6 +157,7 @@ void recieveTransactionDataTest(void)
 	\tTransaction Seq. no.: 1\n\
 	Expected Result: No Error\n\
 	Actual Result: \n");
+
 	strcpy(tD.cardHolderData.cardExpirationDate, "05/22");
 	strcpy(tD.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
 	strcpy(tD.cardHolderData.primaryAccountNumber, "12345678901234567");
@@ -171,6 +179,7 @@ void recieveTransactionDataTest(void)
 	\tTransaction Seq. no.: 2\n\
 	Expected Result: Error\n\
 	Actual Result: ");
+
 	strcpy(tD.cardHolderData.cardExpirationDate, "05/22");
 	strcpy(tD.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
 	strcpy(tD.cardHolderData.primaryAccountNumber, "15349445848148523");
@@ -192,6 +201,7 @@ void recieveTransactionDataTest(void)
 	\tTransaction Seq. no.: 3\n\
 	Expected Result: Error\n\
 	Actual Result: ");
+
 	strcpy(tD.cardHolderData.cardExpirationDate, "05/22");
 	strcpy(tD.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
 	strcpy(tD.terminalData.transactionDate, "16/03/2022");
@@ -213,6 +223,7 @@ void recieveTransactionDataTest(void)
 	\tTransaction Seq. no.: 4\n\
 	Expected Result: Error\n\
 	Actual Result: ");
+
 	strcpy(tD.cardHolderData.cardExpirationDate, "05/22");
 	strcpy(tD.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
 	strcpy(tD.cardHolderData.primaryAccountNumber, "12345678901234567");
@@ -222,6 +233,7 @@ void recieveTransactionDataTest(void)
 	tD.transactionSequenceNumber = 4;
 
 	account_Data->state = BLOCKED;
+
 	recieveTransactionData(&tD);
 }
 
@@ -229,37 +241,39 @@ void isValidAccountTest(void)
 {
 	printf("Tester Name: Youssef Ahmed Mohamed\n\
 	Function Name : isValidAccount \n");
-	ST_cardData_t cardData;
-	ST_accountsDB_t acc;
 
 	printf("Test Case 1:\n\
 	Input Data: CardPAN is in DB \n\
 	Expected Result: No Error\n\
 	Actual Result: ");
-	strcpy(cardData.primaryAccountNumber, "1234");
+
+	strcpy(tD.cardHolderData.primaryAccountNumber, "1234");
 	strcpy(acc.primaryAccountNumber, "1234");
-	isValidAccount(&cardData, &acc);
+
+	isValidAccount(&tD.cardHolderData, &acc);
 
 	printf("\nTest Case 2:\n\
 	Input Data: CardPAN isn't in DB \n\
 	Expected Result: Error\n\
 	Actual Result: ");
-	strcpy(cardData.primaryAccountNumber, "1234");
+
+	strcpy(tD.cardHolderData.primaryAccountNumber, "1234");
 	strcpy(acc.primaryAccountNumber, "1534");
-	isValidAccount(&cardData, &acc);
+
+	isValidAccount(&tD.cardHolderData, &acc);
 }
 
 void isBlockedAccountTest(void)
 {
 	printf("Tester Name: Youssef Ahmed Mohamed\n\
 	Function Name : isBlockedAccount \n");
-	ST_accountsDB_t acc;
 
 	printf("Test Case 1:\n\
 	Input Data: Not Blocked \n\
 	Expected Result: No Error\n\
 	Actual Result: ");
 	acc.state = RUNNING;
+
 	isBlockedAccount(&acc);
 
 	printf("\nTest Case 2:\n\
@@ -267,6 +281,7 @@ void isBlockedAccountTest(void)
 	Expected Result: Error\n\
 	Actual Result: ");
 	acc.state = BLOCKED;
+
 	isBlockedAccount(&acc);
 }
 
@@ -274,44 +289,39 @@ void isAmountAvailableTest(void)
 {
 	printf("Tester Name: Youssef Ahmed Mohamed\n\
 	Function Name : isAmountAvailable \n");
-	ST_terminalData_t term;
-	ST_accountsDB_t acc;
 
 	printf("Test Case 1:\n\
 	Input Data: transAmount = 1234 & Balance  = 12345 \n\
 	Expected Result: No Error\n\
 	Actual Result: ");
-	term.transAmount = 1234;
+
+	tD.terminalData.transAmount = 1234;
 	acc.balance = 12345;
-	isAmountAvailable(&term, &acc);
+
+	isAmountAvailable(&tD.terminalData, &acc);
 
 	printf("\nTest Case 2:\n\
 	Input Data: transAmount = 12345 & Balance  = 1234 \n\
 	Expected Result: Error\n\
 	Actual Result: ");
-	term.transAmount = 12345;
+
+	tD.terminalData.transAmount = 12345;
 	acc.balance = 1234;
-	isAmountAvailable(&term, &acc);
+
+	isAmountAvailable(&tD.terminalData, &acc);
 }
 
 void saveTransactionTest(void)
 {
 	printf("Tester Name: Youssef Ahmed Mohamed\n\
 	Function Name : saveTransaction \n");
-	ST_transaction_t tran;
 	
 	printf("Test Case 1:\n\
 	Input Data: No input data \n\
 	Expected Result: Error\n\
 	Actual Result: ");
-	strcpy(tran.cardHolderData.cardExpirationDate, "\0");
-	strcpy(tran.cardHolderData.cardHolderName, "\0");
-	strcpy(tran.cardHolderData.primaryAccountNumber, "\0");
-	strcpy(tran.terminalData.transactionDate, "\0");
-	tran.terminalData.maxTransAmount = 0;
-	tran.terminalData.transAmount = 0;
-	tran.transactionSequenceNumber = NULL;
-	saveTransaction(&tran);
+
+	saveTransaction(&tD);
 
 	printf("Test Case 2:\n\
 	Input Data: \n\
@@ -325,15 +335,16 @@ void saveTransactionTest(void)
 	\tTransaction State: Approved\n\
 	Expected Result: Print them all\n\
 	Actual Result: \n");
-	strcpy(tran.cardHolderData.cardExpirationDate, "05/22");
-	strcpy(tran.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
-	strcpy(tran.cardHolderData.primaryAccountNumber, "12345678901234567");
-	tran.terminalData.maxTransAmount = 5000.0;
-	strcpy(tran.terminalData.transactionDate, "16/03/2022");
-	tran.terminalData.transAmount = 2000.0;
-	tran.transactionSequenceNumber = 10;
-	tran.transState = APPROVED;
-	saveTransaction(&tran);
+
+	strcpy(tD.cardHolderData.cardExpirationDate, "05/22");
+	strcpy(tD.cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
+	strcpy(tD.cardHolderData.primaryAccountNumber, "12345678901234567");
+	strcpy(tD.terminalData.transactionDate, "16/03/2022");
+	tD.terminalData.maxTransAmount = 5000.0;
+	tD.terminalData.transAmount = 2000.0;
+	tD.transactionSequenceNumber = 10;
+
+	saveTransaction(&tD);
 }
 
 void listSavedTransactionsTest(void)
@@ -345,13 +356,6 @@ void listSavedTransactionsTest(void)
 	Input Data: No input data \n\
 	Expected Result: Nothing will be printed\n\
 	Actual Result: \n");
-	strcpy(trans_Data[0].cardHolderData.cardExpirationDate, "\0");
-	strcpy(trans_Data[0].cardHolderData.cardHolderName, "\0");
-	strcpy(trans_Data[0].cardHolderData.primaryAccountNumber, "\0");
-	trans_Data[0].terminalData.maxTransAmount = 0;
-	strcpy(trans_Data[0].terminalData.transactionDate, "\0");
-	trans_Data[0].terminalData.transAmount = 0;
-	trans_Data[0].transactionSequenceNumber = NULL;
 
 	listSavedTransactions();
 
@@ -377,6 +381,7 @@ void listSavedTransactionsTest(void)
 	\tTransaction State: DECLINED_STOLEN_CARD\n\
 	Expected Result: Print them all\n\
 	Actual Result: \n");
+
 	strcpy(trans_Data[0].cardHolderData.cardExpirationDate, "05/22");
 	strcpy(trans_Data[0].cardHolderData.cardHolderName, "Youssef Ahmed Mohamed");
 	strcpy(trans_Data[0].cardHolderData.primaryAccountNumber, "12345678901234567");
